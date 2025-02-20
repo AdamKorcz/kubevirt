ce2a8f1b0e5f36b82111449732991f40caca7127
Adam Korczynski <adam@adalogics.com>
2025-02-20 21:00:39 +0000

    fik memory leaks in fuzzer
    Signed-off-by: Adam Korczynski <adam@adalogics.com>

--- pkg/virt-controller/watch/drain/evacuation/fuzz_test.go
+++ pkg/virt-controller/watch/drain/evacuation/fuzz_test.go
@@ -1,28 +1,68 @@
 package evacuation
 
 import (
+	stdruntime "runtime"
 	"testing"
 
 	gfh "github.com/AdaLogics/go-fuzz-headers"
 	"github.com/golang/mock/gomock"
 	k8sv1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/fake"
 	k8sTesting "k8s.io/client-go/testing"
 	"k8s.io/client-go/tools/cache"
+	"k8s.io/apimachinery/pkg/util/rand"
 	"k8s.io/client-go/tools/record"
+	virtconfig "kubevirt.io/kubevirt/pkg/virt-config"
 	v1 "kubevirt.io/api/core/v1"
 	"kubevirt.io/client-go/kubecli"
+	virtController "kubevirt.io/kubevirt/pkg/controller"
 	kubevirtfake "kubevirt.io/client-go/kubevirt/fake"
+	framework "k8s.io/client-go/tools/cache/testing"
+	extv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 
 	"kubevirt.io/kubevirt/pkg/testutils"
 )
 
 var (
-	maxResources = 3
+	maxResources = 4
+	kvObjectNamespace = "kubevirt"
+	kvObjectName      = "kubevirt"
 )
 
-// FuzzExecute add up to 3 XXXXXXXXXXXXXXX
+func NewFakeClusterConfigUsingKV(kv *v1.KubeVirt) (*virtconfig.ClusterConfig, cache.SharedIndexInformer, cache.Store, *framework.FakeControllerSource, *framework.FakeControllerSource) {
+	return NewFakeClusterConfigUsingKVWithCPUArch(kv, stdruntime.GOARCH)
+}
+
+func NewFakeClusterConfigUsingKVWithCPUArch(kv *v1.KubeVirt, CPUArch string) (*virtconfig.ClusterConfig, cache.SharedIndexInformer, cache.Store, *framework.FakeControllerSource, *framework.FakeControllerSource) {
+	kv.ResourceVersion = rand.String(10)
+	kv.Status.Phase = "Deployed"
+	crdInformer, cs1 := testutils.NewFakeInformerFor(&extv1.CustomResourceDefinition{})
+	kubeVirtInformer, cs2 := testutils.NewFakeInformerFor(&v1.KubeVirt{})
+
+	kubeVirtInformer.GetStore().Add(kv)
+
+	AddDataVolumeAPI(crdInformer)
+	cfg, _ := virtconfig.NewClusterConfigWithCPUArch(crdInformer, kubeVirtInformer, kvObjectNamespace, CPUArch)
+	return cfg, crdInformer, kubeVirtInformer.GetStore(), cs1, cs2
+}
+
+func AddDataVolumeAPI(crdInformer cache.SharedIndexInformer) {
+	crdInformer.GetStore().Add(&extv1.CustomResourceDefinition{
+		Spec: extv1.CustomResourceDefinitionSpec{
+			Names: extv1.CustomResourceDefinitionNames{
+				Kind: "DataVolume",
+			},
+		},
+	})
+}
+
+func NewFakeClusterConfigUsingKVConfig(kv *v1.KubeVirt) (*virtconfig.ClusterConfig, cache.SharedIndexInformer, cache.Store, *framework.FakeControllerSource, *framework.FakeControllerSource) {
+	return NewFakeClusterConfigUsingKV(kv)
+}
+
+// FuzzExecute add up to 4 resources
 // to the context and then runs the controller.
 func FuzzExecute(f *testing.F) {
 	f.Fuzz(func(t *testing.T, data []byte,
@@ -72,6 +112,10 @@ func FuzzExecute(f *testing.F) {
 			migrations = append(migrations, migration)
 		}
 
+		if len(vmis) + len(nodes) + len(pods) + len(migrations) == 0 {
+			return
+		}
+
 		stop := make(chan struct{})
 		defer close(stop)
 		ctrl := gomock.NewController(t)
@@ -87,15 +131,43 @@ func FuzzExecute(f *testing.F) {
 		migrationInformer, migrationSource := testutils.NewFakeInformerFor(&v1.VirtualMachineInstanceMigration{})
 		nodeInformer, nodeSource := testutils.NewFakeInformerFor(&k8sv1.Node{})
 		podInformer, podSource := testutils.NewFakeInformerFor(&k8sv1.Pod{})
+		defer migrationSource.Shutdown()
+		defer nodeSource.Shutdown()
+		defer podSource.Shutdown()
 		recorder := record.NewFakeRecorder(100)
 		recorder.IncludeObject = true
-		config, _, _ := testutils.NewFakeClusterConfigUsingKVConfig(&v1.KubeVirtConfiguration{})
+
+		kv := &v1.KubeVirt{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      kvObjectName,
+				Namespace: kvObjectNamespace,
+			},
+			Spec: v1.KubeVirtSpec{
+				Configuration: v1.KubeVirtConfiguration{},
+			},
+			Status: v1.KubeVirtStatus{
+				DefaultArchitecture: stdruntime.GOARCH,
+				Phase:               "Deployed",
+			},
+		}
+
+		config, crdInformer, kubeVirtInformerStore, cs1, cs2 := NewFakeClusterConfigUsingKVConfig(kv)
+		defer cs1.Shutdown()
+		defer cs2.Shutdown()
+		defer kubeVirtInformerStore.Delete(kv)
+		defer func(){
+				for _, obj := range crdInformer.GetStore().List() {
+				err := crdInformer.GetStore().Delete(obj)
+				if err != nil {
+					panic(err)
+				}
+			}
+		}()
 
 		controller, _ := NewEvacuationController(vmiInformer, migrationInformer, nodeInformer, podInformer, recorder, virtClient, config)
+		controller.Queue.ShutDown()
 		mockQueue := testutils.NewMockWorkQueue(controller.Queue)
 		controller.Queue = mockQueue
-		migrationFeeder := testutils.NewMigrationFeeder(mockQueue, migrationSource)
-		vmiFeeder := testutils.NewVirtualMachineFeeder(mockQueue, vmiSource)
 
 		// Set up mock client
 		virtClient.EXPECT().VirtualMachineInstanceMigration(k8sv1.NamespaceDefault).Return(fakeVirtClient.KubevirtV1().VirtualMachineInstanceMigrations(k8sv1.NamespaceDefault)).AnyTimes()
@@ -126,25 +198,42 @@ func FuzzExecute(f *testing.F) {
 
 		// Add the resources to the context
 		for _, vmi := range vmis {
-			go vmiFeeder.Add(vmi)
+			key, err := virtController.KeyFunc(vmi)
+			if err != nil {
+				continue
+			}
+			controller.Queue.Add(key)
+			vmiSource.Add(vmi)
 		}
 		for _, node := range nodes {
-			go func() {
-				mockQueue.ExpectAdds(1)
-				nodeSource.Add(node)
-				mockQueue.Wait()
-			}()
+			key, err := virtController.KeyFunc(node)
+			if err != nil {
+				continue
+			}
+			controller.Queue.Add(key)
+			nodeSource.Add(node)
 		}
 		for _, pod := range pods {
-			go func() {
-				mockQueue.ExpectAdds(1)
-				podSource.Add(pod)
-				mockQueue.Wait()
-			}()
+			key, err := virtController.KeyFunc(pod)
+			if err != nil {
+				continue
+			}
+			controller.Queue.Add(key)
+			podSource.Add(pod)
 		}
 		for _, migration := range migrations {
-			go migrationFeeder.Add(migration)
+			key, err := virtController.KeyFunc(migration)
+			if err != nil {
+				continue
+			}
+			controller.Queue.Add(key)
+			migrationSource.Add(migration)
 		}
+		if controller.Queue.Len() == 0 {
+			return
+		}
+		panic("Here")
+
 
 		// Run the controller
 		controller.Execute()
