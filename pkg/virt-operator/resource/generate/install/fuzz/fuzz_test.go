b5c0d2d07bc6f7cdb0eaafcf5608c56dab16e520
Adam Korczynski <adam@adalogics.com>
2025-10-20 19:35:29 +0100

    move fuzzer to fuzz subdirectory
    Signed-off-by: Adam Korczynski <adam@adalogics.com>

--- pkg/virt-operator/resource/generate/install/fuzz/fuzz_test.go
+++ pkg/virt-operator/resource/generate/install/fuzz/fuzz_test.go
@@ -1,4 +1,23 @@
-package install
+/*
+ * This file is part of the KubeVirt project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright The KubeVirt Authors.
+ *
+ */
+
+package fuzz
 
 import (
 	"testing"
@@ -8,6 +27,7 @@ import (
 	"k8s.io/client-go/tools/cache"
 	v1 "kubevirt.io/api/core/v1"
 
+	"kubevirt.io/kubevirt/pkg/virt-operator/resource/generate/install"
 	"kubevirt.io/kubevirt/pkg/virt-operator/util"
 )
 
@@ -49,9 +69,9 @@ func FuzzLoadInstallStrategyFromCache(f *testing.F) {
 			},
 		}
 		if encoded {
-			configMap.Annotations[v1.InstallStrategyConfigMapEncoding] = ManifestsEncodingGzipBase64
+			configMap.Annotations[v1.InstallStrategyConfigMapEncoding] = install.ManifestsEncodingGzipBase64
 		}
 		stores.InstallStrategyConfigMapCache.Add(configMap)
-		_, _ = LoadInstallStrategyFromCache(stores, config)
+		_, _ = install.LoadInstallStrategyFromCache(stores, config)
 	})
 }
